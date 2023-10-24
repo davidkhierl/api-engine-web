bdabc81e5cfe042b9862782aa182f78cf1ec84e5
David Khierl <dkrgidor@gmail.com>
2023-10-24 21:44:34 +0400

    remove button state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-8 p-8">
       <h1>API Engine</h1>
-      <Button asChild icon={<LogIn className="h-4 w-4" />} iconPlacement="right" isLoading>
+      <Button asChild icon={<LogIn className="h-4 w-4" />}>
         <Link href="/login">Login</Link>
       </Button>
       <ThemeModeToggle variant="ghost" />
