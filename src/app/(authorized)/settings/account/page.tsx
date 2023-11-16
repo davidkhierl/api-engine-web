66d5fad2aca4bd7b663104b39843d0f0e8e98d13
David Khierl <dkrgidor@gmail.com>
2023-11-17 00:42:43 +0400

    wip: update layout

--- src/app/(authorized)/settings/account/page.tsx
+++ src/app/(authorized)/settings/account/page.tsx
@@ -1,5 +1,12 @@
 import { WorkInProgress } from '@/app/(authorized)/_components/layouts/work-in-progress'
 
 export default function AccountSettingsPage() {
-  return <WorkInProgress />
+  return (
+    <>
+      <section>
+        <h2 className="mb-4 text-lg">My Account</h2>
+      </section>
+      <WorkInProgress />
+    </>
+  )
 }
